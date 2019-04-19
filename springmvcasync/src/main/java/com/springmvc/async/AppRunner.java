d7b611c7e99330bf8550b30fb33805deff7ed2b6
xuehuiwen <xue95326@163.com>
2019-04-20 00:39:12 +0800

    增加日志

--- springmvcasync/src/main/java/com/springmvc/async/AppRunner.java
+++ springmvcasync/src/main/java/com/springmvc/async/AppRunner.java
@@ -33,5 +33,9 @@ public class AppRunner implements CommandLineRunner {
         logger.info("--> "+ cloudFoundry.get());
         logger.info("--> "+ SpringProjects.get());
 
+        logger.info("--> "+ pivotalSoftware.get());
+        logger.info("--> "+ cloudFoundry.get());
+        logger.info("--> "+ SpringProjects.get());
+
     }
 }
