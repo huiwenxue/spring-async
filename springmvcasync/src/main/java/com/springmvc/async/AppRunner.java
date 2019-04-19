86b3205c2ddaca90bace259ee745a70e2075f03e
xuehuiwen <xue95326@163.com>
2019-04-20 00:59:20 +0800

    我是idea2017

--- springmvcasync/src/main/java/com/springmvc/async/AppRunner.java
+++ springmvcasync/src/main/java/com/springmvc/async/AppRunner.java
@@ -34,5 +34,6 @@ public class AppRunner implements CommandLineRunner {
         logger.info("--> "+ SpringProjects.get());
 
         logger.info("我是idea2018");
+        logger.info("我是idea2017");
     }
 }
