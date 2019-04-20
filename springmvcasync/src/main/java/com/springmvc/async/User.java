6ac97d46799a6a5c2d5989bab4ccd5668adf06df
xuehuiwen <xue95326@163.com>
2019-04-20 23:28:24 +0800

    添加属性

--- springmvcasync/src/main/java/com/springmvc/async/User.java
+++ springmvcasync/src/main/java/com/springmvc/async/User.java
@@ -6,6 +6,7 @@ public class User implements Serializable {
     private String name;
     private String blog;
     private Integer age;
+	private Integer height;
 
     public String getName() {
         return name;
