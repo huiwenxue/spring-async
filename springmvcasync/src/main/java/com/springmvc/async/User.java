cfcba17c77710962a47e12b0e520e5abc89fad6b
xuehuiwen <xue95326@163.com>
2019-04-21 15:55:57 +0800

    添加Lombokjar包

--- springmvcasync/src/main/java/com/springmvc/async/User.java
+++ springmvcasync/src/main/java/com/springmvc/async/User.java
@@ -1,46 +1,11 @@
 package com.springmvc.async;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
+@Data
 public class User implements Serializable {
-    private String name;
-    private String blog;
     private Integer age;
-    private String addr;
-	private Integer height;
-	private Integer count;
-	private Integer number;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getBlog() {
-        return blog;
-    }
-
-    public void setBlog(String blog) {
-        this.blog = blog;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", blog='" + blog + '\'' +
-                ", age=" + age +
-                '}';
-    }
+	private String height;
 }
