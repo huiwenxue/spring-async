3910b967eaeb93ebbc78ca654336c6903d2afc6a
xuehuiwen <xue95326@163.com>
2019-04-21 22:25:14 +0800

    给User类添加序列化ID

--- springmvcasync/src/main/java/com/springmvc/async/User.java
+++ springmvcasync/src/main/java/com/springmvc/async/User.java
@@ -6,6 +6,7 @@ import java.io.Serializable;
 
 @Data
 public class User implements Serializable {
+    private static final long serialVersionUID = -731976207679351696L;
     private Integer age;
 	private String height;
 }
